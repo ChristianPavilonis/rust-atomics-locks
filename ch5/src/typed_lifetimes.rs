c421d1f986577ab9969d667b2e8e7e59d6542eae
Christian <christianpavilonis@outlook.com>
2024-11-01 09:19:55 -0500

    adds a blocking interface

--- ch5/src/typed_lifetimes.rs
+++ ch5/src/typed_lifetimes.rs
@@ -1,22 +1,23 @@
 use std::{
-    cell::UnsafeCell,
-    mem::MaybeUninit,
-    sync::atomic::{AtomicBool, Ordering},
+    cell::UnsafeCell, marker::PhantomData, mem::MaybeUninit, sync::atomic::{AtomicBool, Ordering}, thread::{self, Thread}
 };
 
 pub struct Sender<'a, T> {
     channel: &'a Channel<T>,
+    receiving_thread: Thread,
 }
 
 impl<T> Sender<'_, T> {
     pub fn send(self, message: T) {
         unsafe { (*self.channel.message.get()).write(message) };
-        self.channel.ready.store(true, Ordering::Release)
+        self.channel.ready.store(true, Ordering::Release);
+        self.receiving_thread.unpark();
     }
 }
 
 pub struct Receiver<'a, T> {
     channel: &'a Channel<T>,
+    _no_send: PhantomData<*const ()>,
 }
 
 impl<T> Receiver<'_, T> {
@@ -25,8 +26,9 @@ impl<T> Receiver<'_, T> {
     }
 
     pub fn receive(self) -> T {
-        if !self.channel.ready.swap(false, Ordering::Acquire) {
-            panic!("no message available!");
+        // in case anything else unparks the thread we do a while loop.
+        while !self.channel.ready.swap(false, Ordering::Acquire) {
+            thread::park();
         }
         unsafe { (*self.channel.message.get()).assume_init_read() }
     }
@@ -47,7 +49,7 @@ impl<T> Channel<T> {
 
     pub fn split<'a>(&'a mut self) -> (Sender<'a, T>, Receiver<'a, T>) {
         *self = Self::new();
-        (Sender { channel: self }, Receiver { channel: self })
+        (Sender { channel: self, receiving_thread: thread::current() }, Receiver { channel: self, _no_send: PhantomData })
     }
 }
 
@@ -79,9 +81,6 @@ mod tests {
                 t.unpark();
             });
 
-            while !receiver.is_ready() {
-                thread::park();
-            }
             assert_eq!(receiver.receive(), "hello, world!");
         });
     }
